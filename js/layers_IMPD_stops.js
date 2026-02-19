88b22e89a6bb231e67a9e405bec1412967f01609
Luca Liebscht <124666632+cyclingMoritz@users.noreply.github.com>
2026-02-19 15:42:42 +0100

    Use new structure for pop up data

--- js/layers_IMPD_stops.js
+++ js/layers_IMPD_stops.js
@@ -7,64 +7,7 @@ var mainAttribution = "Map by BSC - DataViz Team";
 //Order by: lower layers first, top layers last
 var layers = [
 
-  {
-    name: "Accessibility Points",
-    sourceLayerName: "bsc_pilot2_all", //source-layer & source
-    attribution:
-      "Accessibility Data <a href='https://opendata-ajuntament.barcelona.cat/data/ca/dataset/accessibilitat-via-publica'>©Ajuntament de Barcelona</a> (<a href='https://creativecommons.org/licenses/by/4.0/deed.ca'>CC BY 4.0</a>)",
-    sourceType: "geojson",
-    layerType: "circle",
-    symbolization: {      
-      "circle-stroke-width": 1,
-      "circle-stroke-color": "#333",
-      "circle-color": [
-        "match",
-        ["get", "Type"],   // property name in your GeoJSON
-        "Obstacles", "hsl(211, 25%, 61%)",
-        "Unevenness", "hsl(120, 40%, 55%)",
-        "Width", "hsl(30, 70%, 60%)",
-        /* default color */ "hsl(0, 0%, 70%)"
-      ],
-      "circle-radius": [
-        "interpolate",
-        ["linear"],
-        ["zoom"],
-        12, 0,
-        15, 5
-      ]
 
-    },
-    legend: {
-      id: "legend-a",
-      class: "legend",
-      items: [
-        {
-          styleHeight: "10px",
-          display: "inline-block",
-          backgroundColor: "hsl(211, 25%, 61%)",
-          range: ["Points"],
-        },
-      ],
-    },
-    states: {
-      visible: "visible",
-      popUps: true,
-      icons: true,
-      filterCat: true,
-      highlight: false,
-      filterLayer: false,
-      dateRange: false,
-    },
-    popUpFeatures: {
-      event: "click",
-      fields: ["Type", "Evaluation", "Value"]
-      // img: "image" // optional, you can add later if needed
-    },
-    filterBy: {
-      active: false,
-      fFeature: "Type",
-    },
-  },
   // Slope
   {
     name: "Slope",
@@ -94,6 +37,7 @@ var layers = [
         15, 5,
         20, 5
       ]
+
 
     },
     legend: {
@@ -135,11 +79,26 @@ var layers = [
       filterLayer: false,
       dateRange: false,
     },
-    popUpFeatures: {
-      event: "click",
-      fields: ["Type", "Evaluation", "Value"]
-      // img: "image" // optional, you can add later if needed
-    },
+    popUps: {
+      "event": "mousemove",
+      "popUpFeatures": [
+        {
+          "type": "keyword",
+          "text": "Type: ",
+          "value": "Type"
+        },
+        {
+          "type": "keyword",
+          "text": "Evaluation: ",
+          "value": "Evaluation"
+        },
+        {
+          "type": "keyword",
+          "text": "Value: ",
+          "value": "value"
+        },
+      ]
+      },
     filterBy: {
       active: false,
       fFeature: "Type",
@@ -210,11 +169,26 @@ var layers = [
       filterLayer: false,
       dateRange: false,
     },
-    popUpFeatures: {
-      event: "click",
-      fields: ["Type", "Evaluation", "Value"]
-      // img: "image" // optional, you can add later if needed
-    },
+    popUps: {
+      "event": "mousemove",
+      "popUpFeatures": [
+        {
+          "type": "keyword",
+          "text": "Type: ",
+          "value": "Type"
+        },
+        {
+          "type": "keyword",
+          "text": "Evaluation: ",
+          "value": "Evaluation"
+        },
+        {
+          "type": "keyword",
+          "text": "Value: ",
+          "value": "value"
+        },
+      ]
+      },
     filterBy: {
       active: false,
       fFeature: "Type",
@@ -284,11 +258,26 @@ var layers = [
       filterLayer: false,
       dateRange: false,
     },
-    popUpFeatures: {
-      event: "click",
-      fields: ["Type", "Evaluation"]
-      // img: "image" // optional, you can add later if needed
-    },
+    popUps: {
+      "event": "mousemove",
+      "popUpFeatures": [
+        {
+          "type": "keyword",
+          "text": "Type: ",
+          "value": "Type"
+        },
+        {
+          "type": "keyword",
+          "text": "Evaluation: ",
+          "value": "Evaluation"
+        },
+        {
+          "type": "keyword",
+          "text": "Value: ",
+          "value": "value"
+        },
+      ]
+      },
     filterBy: {
       active: false,
       fFeature: "Type",
@@ -350,11 +339,26 @@ var layers = [
       filterLayer: false,
       dateRange: false,
     },
-    popUpFeatures: {
-      event: "click",
-      fields: ["Type", "Evaluation"]
-      // img: "image" // optional, you can add later if needed
-    },
+    popUps: {
+      "event": "mousemove",
+      "popUpFeatures": [
+        {
+          "type": "keyword",
+          "text": "Type: ",
+          "value": "Type"
+        },
+        {
+          "type": "keyword",
+          "text": "Evaluation: ",
+          "value": "Evaluation"
+        },
+        {
+          "type": "keyword",
+          "text": "Value: ",
+          "value": "value"
+        },
+      ]
+      },
     filterBy: {
       active: false,
       fFeature: "Type",
@@ -377,7 +381,7 @@ var layers = [
         100, "#1a9641"   // bad (red)
       ],
       "fill-opacity": 0.7,
-      "fill-outline-color": "#333"
+      "fill-outline-color": "#333",
     },
     legend: {
       id: "legend-a",
@@ -396,10 +400,26 @@ var layers = [
       filterLayer: false,
       dateRange: false,
     },
-    popUpFeatures: {
-      event: "click",
-      fields: [ "Name","Number", "Accessibility Score"]
-    },
+    popUps: {
+      "event": "mousemove",
+      "popUpFeatures": [
+        {
+          "type": "keyword",
+          "text": "Type: ",
+          "value": "Type"
+        },
+        {
+          "type": "keyword",
+          "text": "Evaluation: ",
+          "value": "Evaluation"
+        },
+        {
+          "type": "keyword",
+          "text": "Value: ",
+          "value": "value"
+        },
+      ]
+      },
     filterBy: {
       active: false,
       fFeature: "Number",
