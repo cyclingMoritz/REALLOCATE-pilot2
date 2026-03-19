66d1880b226506090d8d5425b5e4e38b964a7ede
Luca Liebscht <124666632+cyclingMoritz@users.noreply.github.com>
2026-03-19 13:45:20 +0100

    Update url

--- map-v5/js/data_retrival.js
+++ map-v5/js/data_retrival.js
@@ -5,17 +5,12 @@ const DATA_RETRIEVAL = {
     RENFE: "#81005E", //Morado
     METRO: "#E72910", //Naranja
   },
-  ICONS: {
-    URL: "https://bizkaia-public-transport-6a487c.gitlab.io/assets/icons/not_my_sprint_v4", 
-    BUS: "bus_green",
-    RENFE: "rail_purple",
-    METRO: "rail-metro_red",
-    GENERIC_MARKER: "marker-stroked",
-  },
-  LOCAL_ICONS: {
-    BUS: "icons/img/bus_green.svg",
-    RENFE: "icons/img/rail_purple.svg",
-    METRO: "icons/img/rail-metro_red.svg",
-    GENERIC_MARKER: "icons/img/marker-stroked.svg",
+ICONS: {
+    URL : "http://127.0.0.1:5500/map-v5/icons/sprite@2x",
+    GENERIC_MARKER: "your-old-marker-name",
+    // ADD YOUR NEW SPRITE NAMES HERE:
+    BUS_STOP_GREEN: "bus-marker-green", // Matches the SVG filename you fed to Spreet
+    BUS_STOP_RED: "bus-marker-red",     // Matches the SVG filename
+    BUS_STOP_DEFAULT: "bus-marker-default"
   }
 }
