0573a1c24227ee6809a1b3dab650089a615373a6
Luca Liebscht <124666632+cyclingMoritz@users.noreply.github.com>
2026-03-19 13:50:59 +0100

    Change sprite url

--- map-v5/js/data_retrival.js
+++ map-v5/js/data_retrival.js
@@ -6,11 +6,6 @@ const DATA_RETRIEVAL = {
     METRO: "#E72910", //Naranja
   },
 ICONS: {
-    URL : "http://127.0.0.1:5500/map-v5/icons/sprite@2x",
-    GENERIC_MARKER: "your-old-marker-name",
-    // ADD YOUR NEW SPRITE NAMES HERE:
-    BUS_STOP_GREEN: "bus-marker-green", // Matches the SVG filename you fed to Spreet
-    BUS_STOP_RED: "bus-marker-red",     // Matches the SVG filename
-    BUS_STOP_DEFAULT: "bus-marker-default"
-  }
+  URL: new URL("icons/sprite@2x", window.location.href).href    
+}
 }
